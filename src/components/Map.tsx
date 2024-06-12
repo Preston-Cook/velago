5061050e3415084f2417c4db2079e18d6b15902a
Preston-Cook <preston.l.cook@gmail.com>
2024-06-11 20:13:07 -0500

    finish market issue on map page

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,24 +3,33 @@
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 import { RecenterMap } from './RecenterMap';
 import { v4 as uuid } from 'uuid';
+import L from 'leaflet';
 
 interface MapProps {
   lat: number;
   lng: number;
 }
 
+const icon = L.icon({
+  iconRetinaUrl: '/images/marker-icon-2x.png',
+  iconUrl: '/images/marker-icon.png',
+  shadowUrl: '/images/marker-shadow.png',
+  iconSize: [30, 47.5],
+  shadowSize: [0, 0],
+});
+
 export default function Map({ lat, lng }: MapProps) {
   return (
     <MapContainer
       center={[30.269501, -97.715942]}
       zoom={10}
-      className="h-full w-full z-20"
+      className="z-20 h-full w-full"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker key={uuid()} position={[lat, lng]} />
+      <Marker icon={icon} key={uuid()} position={[lat, lng]} />
       <RecenterMap lat={lat} lng={lng} />
     </MapContainer>
   );
