9037b8dca83a03dbaa16ee8733c591c8a0f19448
Preston-Cook <preston.l.cook@gmail.com>
2024-06-13 16:36:40 -0500

    add flex row reverse to about page

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Goal, UserRoundPlus, Globe } from 'lucide-react';
+import { Globe, Goal, UserRoundPlus } from 'lucide-react';
 import { v4 as uuid } from 'uuid';
 
 interface AboutSectionProps {
@@ -8,25 +8,31 @@ interface AboutSectionProps {
   iconIdx: number;
 }
 
-const icons = [<Goal key={uuid()} />, <UserRoundPlus key={uuid()} />];
+const icons = [
+  <Goal key={uuid()} />,
+  <UserRoundPlus key={uuid()} />,
+  <Globe key={uuid()} />,
+];
 
 export function AboutSection({ header, text, iconIdx }: AboutSectionProps) {
   const icon = icons[iconIdx as keyof typeof icons];
 
   const smallIcon = React.cloneElement(icon as ReactElement, { size: 27.5 });
-  const largeIcon = React.cloneElement(icon as ReactElement, { size: 180 });
+  const largeIcon = React.cloneElement(icon as ReactElement, { size: 150 });
 
   return (
     <section>
-      <div className="mx-auto flex max-w-screen-xl gap-8 px-4 lg:px-6 lg:py-6">
+      <div
+        className={`mx-auto flex max-w-screen-xl gap-8 px-4 lg:px-6 lg:py-6 ${iconIdx === 1 && 'flex-row-reverse'}`}
+      >
         <div className="sm:text-lg md:w-[60%]">
-          <h2 className="mb-4 flex items-center justify-center gap-2 text-4xl font-bold tracking-tight text-primary">
-            <div className="w-full text-center md:text-left">{header}</div>
+          <h2 className="mb-4 flex flex-row-reverse items-center justify-center gap-2 text-4xl font-bold tracking-tight text-primary">
+            <div className="text-center md:w-full md:text-left">{header}</div>
             <div className="md:hidden">{smallIcon}</div>
           </h2>
           <p className="my-8">&emsp;{text}</p>
         </div>
-        <div className="hidden flex-1 items-start justify-center rounded-lg py-10 text-primary md:flex">
+        <div className="hidden flex-1 items-center justify-center rounded-lg text-primary md:flex">
           <div className="rounded-lg bg-secondary px-5 pb-5 pt-5">
             {largeIcon}
           </div>
