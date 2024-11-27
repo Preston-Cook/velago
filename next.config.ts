28f12956001fc1480818dd76ede5768e08fc2d2a
Preston Cook <prestoncook@MacBookPro.attlocal.net>
2024-11-26 18:56:03 -0600

    add checks for environment variables before starting in prod and development

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
+import { config } from 'dotenv-safe';
 import createNextIntlPlugin from 'next-intl/plugin';
 
+config({
+  path:
+    process.env.NODE_ENV === 'development'
+      ? '.env.development'
+      : '.env.production',
+});
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
